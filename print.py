3b8d12ae1455cc353e5c5dc3194901d206afb35f
muneeb <mu556031@gmail.com>
2024-10-14 12:56:01 +0800

    printing and writing on the screen list

--- print.py
+++ print.py
@@ -11,13 +11,22 @@ def defScreen(screen, screenRow, screenColumn):
 
     for i in range(screenRow):
         for j in range(screenColumn):
-            screen[i][j] = x
+            if (i == 0 or i == screenRow - 1 ):
+                screen[i][j] = '='
+            elif (j == 0 or j == screenColumn - 1 ):
+                screen[i][j] = '|'
+            else:
+                screen[i][j] = x
 
 def writeText(screen, screenRow, screenColumn, text, percentLeft, percentTop):
 
+    if (percentTop >= 1 or percentLeft >= 1 or percentLeft <= 0 or percentTop <= 0):
+        print("error: use percentTop, percentLeft with range of 0 to 1")
+        return
+
     # error checking for text longer than the screen
-    if (len(text) > screenColumn):
-        print("error please increase the size of screen")
+    if (len(text) > screenColumn - 2):
+        print("error:  please increase the size of screen")
         return
 
     #checking row and columns for the writing
--- test.py
+++ test.py
@@ -5,13 +5,15 @@ from print import *
 #setting array for the screen
 #screen is about 50 by 50 characters which will be changed later or improved
 
-screenRow = 20
-screenColumn = 20
+screenRow = 5
+screenColumn = 5
 
 screen = [['*' for i in range(screenRow)] for j in range(screenColumn)]
 
 defScreen(screen, screenRow, screenColumn)
 
-writeText(screen, screenRow, screenColumn, "ColDol", 0.5, 0.3)
+writeText(screen, screenRow, screenColumn, "x  y", 5/10, 3/10)
+
 
 printScreen(screen, screenRow,screenColumn)
+
